7c5c8b68c9524442942ef271e2daf38a887b333f
Amna Fatima <amnafatimaa6@gmail.com>
2026-03-05 22:40:39 -0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 import pandas as pd
 import numpy as np
 import streamlit as st
-import matplotlib.pyplot as plt
 import seaborn as sns
 
 from sklearn.linear_model import LinearRegression
