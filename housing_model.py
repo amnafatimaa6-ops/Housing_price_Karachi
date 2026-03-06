f01b96e7edd2151bcb43e48fbf57ade9faa6fd0d
Amna Fatima <amnafatimaa6@gmail.com>
2026-03-05 20:54:05 -0800

    Update housing_model.py

--- housing_model.py
+++ housing_model.py
@@ -3,13 +3,7 @@
 # This file should NOT load the dataset itself.
 # The dataset (df) will be passed from app.py
 
-import pandas as pd
-import numpy as np
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor
 
 
 def run_models(df):
